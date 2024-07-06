782966188c0e30a4859d98eb435e2028cc7001bd
Aseem5047 <aseemgupta2002@gmail.com>
2024-07-06 13:07:40 +0530

    updated changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import "./globals.css";
 import { Metadata } from "next";
 import Script from "next/script";
-import { GoogleAnalytics } from "nextjs-google-analytics";
 
 export const metadata: Metadata = {
 	title: "Flashcall",
@@ -18,27 +17,21 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			{/* <head>
+			<head>
 				<Script
+					src={`https://www.googletagmanager.com/gtag/js?id=G-1GY673B37C`}
 					strategy="afterInteractive"
-					async
-					src="https://www.googletagmanager.com/gtag/js?id=G-1GY673B37C"
-				></Script>
+				/>
 				<Script id="google-analytics" strategy="afterInteractive">
 					{`
 						window.dataLayer = window.dataLayer || [];
 						function gtag(){dataLayer.push(arguments);}
 						gtag('js', new Date());
-						gtag('config', 'G-1GY673B37C', {
-						'cookie_domain': 'flashcall.me'
-					});
+
+						gtag('config', 'G-1GY673B37C');
 					`}
 				</Script>
-			</head> */}
-			<GoogleAnalytics
-				gaMeasurementId="G-1GY673B37C"
-				strategy="afterInteractive"
-			/>
+			</head>
 			<body className="no-scrollbar overflow-x-clip">{children}</body>
 		</html>
 	);
