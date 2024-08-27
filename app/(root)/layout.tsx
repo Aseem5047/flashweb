8a10ad05c72402e754eeb43c147991653c613c90
Aseem5047 <aseemgupta2002@gmail.com>
2024-08-27 18:39:36 +0530

    updated logic

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,23 +2,10 @@
 "use client";
 
 import Footer from "@/components/shared/Footer";
-import Loader from "@/components/shared/Loader";
 import Navbar from "@/components/shared/Navbar";
 import React, { ReactNode, useEffect, useState } from "react";
 
 const ClientLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
-	const [loading, setLoading] = useState(true);
-
-	useEffect(() => {
-		const timer = setTimeout(() => {
-			setLoading(false);
-		}, 2000);
-
-		return () => clearTimeout(timer);
-	}, []);
-
-	if (loading) return <Loader />;
-
 	return (
 		<div className="relative md:bg-hero bg-contain no-scrollbar">
 			<Navbar />
